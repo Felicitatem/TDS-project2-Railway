50ba3c84ebe2c1cba16da16bb472e02e0e866166
Felicitatem <kashyapsamriddhi4@gmail.com>
2025-08-20 02:36:11 +0530

    Final commit with Railway deployment config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-uvicorn main:app --host 0.0.0.0 --port 8000
+# entrypoint.sh
+
+# Use the PORT environment variable if it's set, otherwise default to 8000
+UVICORN_PORT=${PORT:-8000}
+
+# Start the Uvicorn server
+uvicorn main:app --host 0.0.0.0 --port $UVICORN_PORT
